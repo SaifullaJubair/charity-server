2aaa74106175659ba3ae51b8fcd957241a094288
SaifullaJubair <saifullajubair6@gmail.com>
2024-02-07 01:59:25 +0600

    login user api create and jwt token implement

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
@@ -33,8 +34,37 @@ const run = async () => {
         .toArray();
       res.send(result);
     });
+
+    // Get Single User login
+    app.post("/login", async (req, res) => {
+      try {
+        const { email, password } = req.body;
+        const user = await usersCollection.findOne({ email });
+        // Check if the user exists
+        if (!user) {
+          return res.status(401).json({ error: "Invalid email or password" });
+        }
+        // Check if the password is correct
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid) {
+          return res.status(401).json({ error: "Invalid email or password" });
+        }
+        const token = jwt.sign(
+          { _id: user._id, email: user.email },
+          process.env.JWT_SECRET,
+          {
+            expiresIn: "30d",
+          }
+        );
+
+        res.status(200).json({ message: "Login successful", token });
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Internal server error" });
+      }
+    });
     // Create User
-    app.post("/users", async (req, res) => {
+    app.post("/register", async (req, res) => {
       try {
         // date format
         const currentDate = new Date();
